356ebfacd8c4cd04fd2c885aa69f34d80703ebd7
Brian Li <brianli@master-dynamic.com>
2022-12-19 11:27:33 +0800

    modified:   src/api.tsx

--- src/api.tsx
+++ src/api.tsx
@@ -4,13 +4,13 @@ function api() {
 }
 
 export const getTemperature = async (): Promise<interfaces.sensor[]> => {
-    const response = await fetch('http://192.168.3.105:8080/api/sensors');
+    const response = await fetch('http://192.168.3.105/api/sensors');
     const sensors:interfaces.sensor[] = await response.json()
     return sensors;
 }
 
 export const getZones = async ():Promise<interfaces.zone[]> =>{
-    const response = await fetch('http://192.168.3.105:8080/api/v1/sensors');
+    const response = await fetch('http://192.168.3.105/api/v1/sensors');
     const zones:interfaces.zone[] = await response.json()
     return zones;
 
