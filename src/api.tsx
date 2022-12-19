c749e948d75f6018e4d12d17d665e2190acdaf6f
Brian Li <brianli@master-dynamic.com>
2022-12-19 15:55:38 +0800

    Revert "	modified:   src/api.tsx"
    This reverts commit 720c93e8ec55ca898531f7fcc7bcf4073f66e6f8.

--- src/api.tsx
+++ src/api.tsx
@@ -36,20 +36,15 @@ export const fetchTemperature = async (url: string): Promise<interfaces.temperat
 }
 
 export const getTemperature = async (): Promise<interfaces.temperatureSensorValue[]> => {
-    let requestUrl = "http://192.168.3.105/api/v1/realtime/sensor?"
-
-    let response = await fetch('http://192.168.3.105/api/v1/sensors');
-    const sensors: interfaces.sensor[] = await response.json();
-    sensors.forEach((sensor) => {
-        if (sensor.id.includes("IAQsensor")) {
-            requestUrl += "&id=" + sensor.id;
-        }
+    let temperatureValues: interfaces.temperatureSensorValue[] = []
+    await getTemperatureSensorUrl().then(async (url: string) => {
+        await console.log(url)
+        fetchTemperature(url).then(async (temperatureSensors: interfaces.temperatureSensorValue[]) => {
+            await console.log(temperatureSensors);
+            temperatureValues = temperatureSensors;
+        })
     })
-
-    response = await fetch(requestUrl);
-    const temperatures = response.json();
-
-    await console.log(temperatures)
-    return temperatures
+    await console.log(temperatureValues)
+    return temperatureValues
 }
 export default api;
